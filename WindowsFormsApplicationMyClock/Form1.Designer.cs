4c3b84585e2d573a815d69182fe07bdb27eec4d4
Miracle <rjcvjchzljv9@gmail.com>
2020-02-13 03:29:13 +0200

    Done!

--- WindowsFormsApplicationMyClock/Form1.Designer.cs
+++ WindowsFormsApplicationMyClock/Form1.Designer.cs
@@ -28,22 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.startPauseButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelSec = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // startPauseButton
+            // 
+            this.startPauseButton.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startPauseButton.Location = new System.Drawing.Point(12, 76);
+            this.startPauseButton.Name = "startPauseButton";
+            this.startPauseButton.Size = new System.Drawing.Size(75, 43);
+            this.startPauseButton.TabIndex = 0;
+            this.startPauseButton.Text = "start";
+            this.startPauseButton.UseVisualStyleBackColor = true;
+            this.startPauseButton.Click += new System.EventHandler(this.startPauseButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "resset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelSec
+            // 
+            this.labelSec.AutoSize = true;
+            this.labelSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSec.Location = new System.Drawing.Point(12, 32);
+            this.labelSec.Name = "labelSec";
+            this.labelSec.Size = new System.Drawing.Size(20, 24);
+            this.labelSec.TabIndex = 2;
+            this.labelSec.Text = "0";
+            this.labelSec.Click += new System.EventHandler(this.labelSec_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TIME";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(239, 161);
+            this.ClientSize = new System.Drawing.Size(378, 180);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelSec);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.startPauseButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startPauseButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelSec;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApplicationMyClock/Form1.cs
+++ WindowsFormsApplicationMyClock/Form1.cs
@@ -12,15 +12,36 @@ namespace WindowsFormsApplicationMyClock
 {
     public partial class Form1 : Form
     {
-        Timer timer = new Timer();
+        private Timer timer = new Timer();
+        private Timer timerSec = new Timer();
+        private bool trigger = false;
+        private int second = 0;
+        private int min = 0;
+        DateTime dt = new DateTime();
+        
+
+
         public Form1()
         {
             InitializeComponent();
+            timerSec.Interval = 1000;
+            timerSec.Tick += (o, e) =>
+            {
+                second++;
+                if (second >= 60)
+                {
+                    min++;
+                    second = 0;
+                }
+                labelSec.Text = min.ToString() + "m " + second.ToString() + "sec";
+                Invalidate();
+            };
+            labelSec.Text = "0m 0sec";
             timer.Interval = 1000;
             DoubleBuffered = true;
             timer.Tick += DoTime;
             timer.Start();
-            Text = "MyClock";
+            Text = "Clock";
             SetStyle(ControlStyles.ResizeRedraw, true);
         }
 
@@ -37,11 +58,41 @@ namespace WindowsFormsApplicationMyClock
         private void InitializeTransform(Graphics g)
         {
             g.ResetTransform();
-            g.TranslateTransform(ClientSize.Width / 2, ClientSize.Height / 2);
+            g.TranslateTransform(ClientSize.Width / 2f, ClientSize.Height / 2f);
             float scale = System.Math.Min(ClientSize.Width, ClientSize.Height) / 200.0f;
             g.ScaleTransform(scale, scale);
         }
+        private void InitializeTransformForSec(Graphics g)
+        {
+            g.ResetTransform();
+            g.TranslateTransform(ClientSize.Width / 2f + 90f + 45f, ClientSize.Height / 2f + 35f);
+            float scale = System.Math.Min(ClientSize.Width, ClientSize.Height) / 200.0f;
+            //g.ScaleTransform(scale, scale);
+        }
+        //Секундомер
+        private void Sec(Graphics g, SolidBrush brush)
+        {
+            InitializeTransformForSec(g);
+            for (int i = 0; i < 72; i++)
+            {
+                g.RotateTransform(5.0f);
+                g.FillRectangle(brush, 50, -5, 5, 5);
+            }
+            InitializeTransformForSec(g);
+            for (int i = 0; i < 60; i++)
+            {
+                g.RotateTransform(6.0f);
+                g.FillRectangle(brush, 45, -1, 10, 2);
+            }
+            //Стрелка
+            InitializeTransformForSec(g);
+            
+            g.RotateTransform(second * 6.0f);
 
+            DrawHand(g, brush, 45, true);
+
+
+        }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
@@ -83,14 +134,16 @@ namespace WindowsFormsApplicationMyClock
             g.RotateTransform(secondInt * 6f);
             DrawHand(g, green, 85, true);
             InitializeTransform(g);
+
+            //Дорисовываем секундомер
+            Sec(g, black);
+
             //освобождение ресурсов
             green.Dispose();
             black.Dispose();
             blue.Dispose();
             yellow.Dispose();
             red.Dispose();
-
-
         }
         private void DrawHand(Graphics g, SolidBrush solidBrush, int length, bool seen)
         {
@@ -104,6 +157,39 @@ namespace WindowsFormsApplicationMyClock
             points[3].X = (seen) ? 2 : 10;
             points[3].Y = 0;
             g.FillPolygon(solidBrush, points);
+        }
+
+        private void startPauseButton_Click(object sender, EventArgs e)
+        {
+            if (!trigger)
+            {
+                timerSec.Start();
+                startPauseButton.Text = "pause";
+                trigger = true;
+            }
+            else if (trigger)
+            {
+                timerSec.Stop();
+                startPauseButton.Text = "start";
+                trigger = false;
+            }
+            Invalidate();
+        }
+
+        private void labelSec_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timerSec.Stop();
+            min = 0;
+            second = 0;
+            trigger = false;
+            startPauseButton.Text = "start";
+            labelSec.Text = "0m 0sec";
+            Invalidate();
         }
     }
 }
